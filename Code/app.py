7de385cd89af7b5cb6dde0f426d5aec015551912
Lee Lihua <lihua.lee.2017@sis.smu.edu.sg>
2020-04-01 22:20:48 +0800

    updated models and app.py for api

--- Code/app.py
+++ Code/app.py
@@ -2,6 +2,8 @@
 # Step 01: import necessary libraries/modules
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+import json
 
 
 # Step 02: initialize flask app here 
@@ -28,75 +30,108 @@ def get_visitorship():
 		return jsonify(cap.serialize())
 	else:
 		caps = Visitorship.query.all() 
-		return jsonify([c.serialize() for c in caps])	
+		return jsonify([c.serialize() for c in caps])
+
 
 @app.route('/getHourlyData/', methods=['GET'])
 def get_hourlydata():
 	# query data based on gender, start_hour, end_hour.
 	if 'reportDate' in request.args:
-		reportDate = (request.args.get('reportDate'))
-		hour = Report_hour.query.filter_by(report_date=reportDate).first()
-		return jsonify(hour.serialize())
+		reportDate = request.args.get('reportDate')
+		print(reportDate)
+		hours = Report_hour.query.filter_by(report_date=reportDate)
+		return jsonify([h.serialize() for h in hours])
 	
-	if 'gender' and 'hour' in request.args:
+	if 'gender' in request.args:
 		gender = (request.args.get('gender'))
-		hour = Report_hour.query.filter_by(gender=gender).first()
+		hours = Report_hour.query.filter_by(gender=gender)
+		return jsonify([h.serialize() for h in hours])
+	
+	if 'start_hour' in request.args:
+		start_hour = (request.args.get('start_hour'))
+		print(start_hour)
+		hours = Report_hour.query.filter_by(hour=start_hour)
+		return jsonify([h.serialize() for h in hours])
 		
 	else:
 		hours = Report_hour.query.all() 
 		return jsonify([h.serialize() for h in hours])
 
-#  allows the user to determine which day (and/or hour) of the week is the busiest, so that they know what is the best timing/day to visit the gym
+#  allows the user to determine which day (and/or hour) of the week is the busiest, 
+#  so that they know what is the best timing/day to visit the gym
+@app.route('/getWeeklyHourly/', methods=['GET']) 
+def get_busyday():
+	if 'start_date' in request.args and 'end_date' in request.args: 
+		#start and end date, return capacity for the week
+		start = request.args.get('start_date')
+		end = request.args.get('end_date')
+		hours = Report_hour.query.filter_by(report_date=reportDate) #between start and end, visitorship!!!
+		return jsonify([h.serialize() for h in hours])
+
+	if 'start_date' in request.args and 'end_date' in request.args and 'hour' in request.args:
+		# between start and end date, hour -> report_hour
+		start = request.args.get('start_date')
+
 # "prediction" of whether a particular day of the week and time will be busy (based on historical data)
+@app.route('/getprediction/', methods=['GET']) 
+def get_prediction():
+	if 'start_date' in request.args and 'end_date' in request.args: # day and time 
+		# start_date, end_date and time -> add up female and male 
+		# get date where start_date and end_date + max(cap)
+		# get hour where date = max(cap) and max(cap) -> M+F
+		# select * from report_date where report_date between start and end + max(capacity)
+
+		# return full or empty
+		start = request.args.get('start_date')
+
+
 
 @app.route('/reporthour', methods=['POST'])
 def create_reporthour():
 
-	report_date = request.json['date']
-	hour = request.json['hour']
-	gender = request.json['gender']
-	capacity = request.json['capacity']
+	# report_date = request.json['date']
+	# hour = request.json['hour']
+	# gender = request.json['gender']
+	# capacity = request.json['capacity']
+	count=0
+	hours = request.json['report_hours']
 
 	try:
-		new_hour = Report_hour(hour=hour,gender=gender,capacity=capacity,report_date = report_date)
-		db.session.add(new_hour)
-		db.session.commit() 
-	
-		return jsonify('new hour record {} was created for user {}'.format(hour, gender))
+		for each in hours:
+			report_date = each['report_date']
+			hour = each['hour']
+			gender = each['gender']
+			capacity = each['capacity']
+			new_hour = Report_hour(hour=hour,gender=gender,capacity=capacity,report_date = report_date)
+			db.session.add(new_hour)
+			db.session.commit() 
+			count+=1
+
+		return jsonify('{} record added'.format(count))
 
 	except Exception as e:
 		return (str(e))
 
 @app.route('/visitor', methods=['POST'])
 def create_visitor():
-
-	report_date = request.json['date']
-	capacity = request.json['capacity']
+	count = 0 
+	visitors = request.json['visitors']
+	# report_date = request.json['date']
+	# capacity = request.json['capacity']
 
 	try:
-		new_day = Visitorship(report_date = report_date,total_capacity=capacity)
-		db.session.add(new_day)
-		db.session.commit() 
+		for v in visitors:
+			report_date = v['report_date']
+			capacity = v['total_capacity']
+			new_day = Visitorship(report_date = report_date,total_capacity=capacity)
+			db.session.add(new_day)
+			db.session.commit()
+			count += 1 
 	
-		return jsonify('new hour record {} was created for user {}'.format(report_date, capacity))
+		return jsonify('{} record added'.format(count))
 
 	except Exception as e:
 		return (str(e))
-
-@app.route('/visitorship/<date:date>', methods=['DEL'])
-def del_all():
-	try:
-		# visit = Visitorship.query.delete()
-		# db.session.query(Visitorship).delete()
-		# db.session.delete(visit)
-		visit = Visitorship.query.get(date)
-		db.session.delete(visit)
-		db.session.commit()
-	except:
-		db.session.rollback()
-
-	# return jsonify('{} rows was deleted'.format())
- 	
 
 # your code ends here 
 
--- Code/models.py
+++ Code/models.py
@@ -1,38 +1,45 @@
-import datetime
+from datetime import datetime
 
 from app import db
 from sqlalchemy.dialects.postgresql import TIME
+import datetime
+from sqlalchemy.types import TypeDecorator, TIMESTAMP
 
 
 class Visitorship(db.Model):
 	__tablename__ = 'visitorship'
 
-	report_date = db.Column(db.Date, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True) 
+	report_date = db.Column(db.Date, unique=True)
 	total_capacity = db.Column(db.Integer(), nullable=False)
 
 	# ONE-to-MANY relationship model - Each visitor may have multiple report hour; each report hour is tagged to only one visitor.
 	visit_hour = db.relationship('Report_hour', back_populates='hour_per_visitor', uselist=True, cascade='all, delete-orphan', lazy=True)
-	
-	def __init__(self, report_date, total_capacity):
+
+
+	def __init__(self, report_date, total_capacity, visit_hour=None):
 		self.report_date = report_date
 		self.total_capacity = total_capacity
-
+		self.visit_hour = [] if visit_hour is None else visit_hour
 
 	def serialize(self):
 		return {
-			'reportDate': self.report_date, 
+			'reportDate': self.report_date.strftime('%d-%m-%y'), 
 			'totalCapacity': self.total_capacity,
+			'response':[{"gender":v.gender, "capacity":v.capacity, "hour": v.hour.strftime('%H:%M')} for v in self.visit_hour]
 		}
+
 
 
 class Report_hour(db.Model):
 	__tablename__ = 'report_hour'
 
-	# hour have to use datetime format first then trancate the time after that
 	# date = db.Column(db.DateTime, primary_key=True)
-	hour = db.Column(db.Time, primary_key=True)
-	gender = db.Column(db.String(80), primary_key=True)
-	capacity = db.Column(db.Integer, unique=False, nullable=False)
+	id = db.Column(db.Integer, primary_key=True) 
+	hour = db.Column(db.Time, nullable=False)
+	gender = db.Column(db.String(1), nullable=False)
+	capacity = db.Column(db.Integer, nullable=False)
+
 	report_date = db.Column(db.Date, db.ForeignKey('visitorship.report_date'), nullable=False)
 
 	hour_per_visitor = db.relationship('Visitorship', back_populates='visit_hour')
@@ -46,14 +53,12 @@ class Report_hour(db.Model):
 
 	def serialize(self): #not sure if this is right
 			return {
-				'reportDate': self.report_date, 
-				'response': [
-					{
-						"gender": self.gender,
-						"capacity": self.capacity
-					}
-				]
+				'reportDate': self.report_date.strftime('%d-%m-%y'),
+				'hour': self.hour.strftime('%H:%M'),
+				'gender': self.gender,
+				'capacity': self.capacity
 			}
+	
 
 
 
